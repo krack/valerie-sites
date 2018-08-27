3864d8e7f37bab0ffda5a7d2661db01d255b15a8
Sylvain Gandon <sylvain.gandon@soprasteria.com>
2018-08-27 13:53:56 +0200

    update with Valérie about menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,202 +1,193 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { environment } from '../environments/environment';
-
 import { AccueilFestyComponent } from './accueil-festy/accueil-festy.component';
 import { AccueilGourmandsComponent } from './accueil-gourmands/accueil-gourmands.component';
-import { ConceptFestyComponent } from './concept-festy/concept-festy.component';
-import { RepasOuvriersComponent } from './repas-ouvriers/repas-ouvriers.component';
-
 import { AperitifComponent } from './aperitif/aperitif.component';
-import { MenuFestyComponent } from './menu-festy/menu-festy.component';
-import { RepasGroupesComponent } from './repas-groupes/repas-groupes.component';
-
-/*menus import */
-import { MenuLouiseComponent } from './menus/menu-louise/menu-louise.component';
-import { MenuAgatheComponent } from './menus/menu-agathe/menu-agathe.component';
-import { MenuBertilleComponent } from './menus/menu-bertille/menu-bertille.component';
-import { MenuAliceComponent } from './menus/menu-alice/menu-alice.component';
-import { DessertsComponent } from './menus/desserts/desserts.component';
-/* buffet import */
-import { BuffetsComponent } from './buffets/buffets.component';
+import { AutresServicesComponent } from './autres-services/autres-services.component';
+import { BarEpicerieComponent } from './bar-epicerie/bar-epicerie.component';
 import { BuffetJeanneComponent } from './buffets/buffet-jeanne/buffet-jeanne.component';
 import { BuffetJustineComponent } from './buffets/buffet-justine/buffet-justine.component';
 import { BuffetLinaComponent } from './buffets/buffet-lina/buffet-lina.component';
 import { BuffetMarieComponent } from './buffets/buffet-marie/buffet-marie.component';
-import { BuffetAutreComponent } from './buffets/buffet-autre/buffet-autre.component';
-
-import { AutresServicesComponent } from './autres-services/autres-services.component';
-import { BarEpicerieComponent } from './bar-epicerie/bar-epicerie.component';
-
+/* buffet import */
+import { BuffetsComponent } from './buffets/buffets.component';
+import { ConceptFestyComponent } from './concept-festy/concept-festy.component';
 import { ContactComponent } from './contact/contact.component';
+import { MenuFestyComponent } from './menu-festy/menu-festy.component';
+import { DessertsComponent } from './menus/desserts/desserts.component';
+import { MenuAgatheComponent } from './menus/menu-agathe/menu-agathe.component';
+import { MenuAliceComponent } from './menus/menu-alice/menu-alice.component';
+import { MenuBertilleComponent } from './menus/menu-bertille/menu-bertille.component';
+/*menus import */
+import { MenuLouiseComponent } from './menus/menu-louise/menu-louise.component';
+import { RepasGroupesComponent } from './repas-groupes/repas-groupes.component';
+import { RepasOuvriersComponent } from './repas-ouvriers/repas-ouvriers.component';
+
+
+
+
+
 
 const routes: Routes = [
 ];
 
 if (environment.siteNumber == 1) {
-	routes.push({
-		path: 'accueil',
-		component: AccueilFestyComponent
-	});
-	routes.push({
-		path: 'concept',
-		component: ConceptFestyComponent
-	});
-	routes.push({
-		path: 'aperitif',
-		component: AperitifComponent
-	});
-	routes.push({
-		path: 'menus',
-		component: MenuFestyComponent
-	});
-	routes.push({
-		path: 'menus/louise',
-		component: MenuLouiseComponent
-	});
-	routes.push({
-		path: 'menus/agathe',
-		component: MenuAgatheComponent
-	});
-	routes.push({
-		path: 'menus/bertille',
-		component: MenuBertilleComponent
-	});
-	routes.push({
-		path: 'menus/alice',
-		component: MenuAliceComponent
-	});
-	routes.push({
-		path: 'menus/desserts',
-		component: DessertsComponent
-	});
+  routes.push({
+    path: 'accueil',
+    component: AccueilFestyComponent
+  });
+  routes.push({
+    path: 'concept',
+    component: ConceptFestyComponent
+  });
+  routes.push({
+    path: 'aperitif',
+    component: AperitifComponent
+  });
+  routes.push({
+    path: 'menus',
+    component: MenuFestyComponent
+  });
+  routes.push({
+    path: 'menus/louise',
+    component: MenuLouiseComponent
+  });
+  routes.push({
+    path: 'menus/agathe',
+    component: MenuAgatheComponent
+  });
+  routes.push({
+    path: 'menus/bertille',
+    component: MenuBertilleComponent
+  });
+  routes.push({
+    path: 'menus/alice',
+    component: MenuAliceComponent
+  });
+  routes.push({
+    path: 'menus/desserts',
+    component: DessertsComponent
+  });
 
-	routes.push({
-		path: 'buffets',
-		component: BuffetsComponent
-	});
-	routes.push({
-		path: 'buffets/jeanne',
-		component: BuffetJeanneComponent
-	});
-	routes.push({
-		path: 'buffets/justine',
-		component: BuffetJustineComponent
-	});
-	routes.push({
-		path: 'buffets/lina',
-		component: BuffetLinaComponent
-	});
-	routes.push({
-		path: 'buffets/marie',
-		component: BuffetMarieComponent
-	});
-	routes.push({
-		path: 'buffets/autre',
-		component: BuffetAutreComponent
-	});
-	routes.push({
-		path: 'autre-services',
-		component: AutresServicesComponent
-	});
+  routes.push({
+    path: 'buffets',
+    component: BuffetsComponent
+  });
+  routes.push({
+    path: 'buffets/jeanne',
+    component: BuffetJeanneComponent
+  });
+  routes.push({
+    path: 'buffets/justine',
+    component: BuffetJustineComponent
+  });
+  routes.push({
+    path: 'buffets/lina',
+    component: BuffetLinaComponent
+  });
+  routes.push({
+    path: 'buffets/marie',
+    component: BuffetMarieComponent
+  });
+  routes.push({
+    path: 'autre-services',
+    component: AutresServicesComponent
+  });
 
-	routes.push({
-		path: 'contact',
-		component: ContactComponent
-	});
+  routes.push({
+    path: 'contact',
+    component: ContactComponent
+  });
 
-	routes.push({
-		path: '',
-		redirectTo: '/accueil',
-		pathMatch: 'full'
-	});
+  routes.push({
+    path: '',
+    redirectTo: '/accueil',
+    pathMatch: 'full'
+  });
 
 } else if (environment.siteNumber == 2) {
-	routes.push({
-		path: 'accueil',
-		component: AccueilGourmandsComponent
-	});
-	routes.push({
-		path: 'restaurant',
-		component: RepasOuvriersComponent
-	});
+  routes.push({
+    path: 'accueil',
+    component: AccueilGourmandsComponent
+  });
+  routes.push({
+    path: 'restaurant',
+    component: RepasOuvriersComponent
+  });
 
-	routes.push({
-		path: 'repas-groupes',
-		component: RepasGroupesComponent
-	});
-	routes.push({
-		path: 'aperitif',
-		component: AperitifComponent
-	});
+  routes.push({
+    path: 'repas-groupes',
+    component: RepasGroupesComponent
+  });
+  routes.push({
+    path: 'aperitif',
+    component: AperitifComponent
+  });
 
-	routes.push({
-		path: 'menus/louise',
-		component: MenuLouiseComponent
-	});
-	routes.push({
-		path: 'menus/agathe',
-		component: MenuAgatheComponent
-	});
-	routes.push({
-		path: 'menus/bertille',
-		component: MenuBertilleComponent
-	});
-	routes.push({
-		path: 'menus/alice',
-		component: MenuAliceComponent
-	});
-	routes.push({
-		path: 'menus/desserts',
-		component: DessertsComponent
-	});
+  routes.push({
+    path: 'menus/louise',
+    component: MenuLouiseComponent
+  });
+  routes.push({
+    path: 'menus/agathe',
+    component: MenuAgatheComponent
+  });
+  routes.push({
+    path: 'menus/bertille',
+    component: MenuBertilleComponent
+  });
+  routes.push({
+    path: 'menus/alice',
+    component: MenuAliceComponent
+  });
+  routes.push({
+    path: 'menus/desserts',
+    component: DessertsComponent
+  });
 
-	routes.push({
-		path: 'buffets',
-		component: BuffetsComponent
-	});
-	routes.push({
-		path: 'buffets/jeanne',
-		component: BuffetJeanneComponent
-	});
-	routes.push({
-		path: 'buffets/justine',
-		component: BuffetJustineComponent
-	});
-	routes.push({
-		path: 'buffets/lina',
-		component: BuffetLinaComponent
-	});
-	routes.push({
-		path: 'buffets/marie',
-		component: BuffetMarieComponent
-	});
-	routes.push({
-		path: 'buffets/autre',
-		component: BuffetAutreComponent
-	});
-	routes.push({
-		path: 'bar-epicerie',
-		component: BarEpicerieComponent
-	});
+  routes.push({
+    path: 'buffets',
+    component: BuffetsComponent
+  });
+  routes.push({
+    path: 'buffets/jeanne',
+    component: BuffetJeanneComponent
+  });
+  routes.push({
+    path: 'buffets/justine',
+    component: BuffetJustineComponent
+  });
+  routes.push({
+    path: 'buffets/lina',
+    component: BuffetLinaComponent
+  });
+  routes.push({
+    path: 'buffets/marie',
+    component: BuffetMarieComponent
+  });
+  routes.push({
+    path: 'bar-epicerie',
+    component: BarEpicerieComponent
+  });
 
-	routes.push({
-		path: 'contact',
-		component: ContactComponent
-	});
+  routes.push({
+    path: 'contact',
+    component: ContactComponent
+  });
 
-	routes.push({
-		path: '',
-		redirectTo: '/accueil',
-		pathMatch: 'full'
-	});
+  routes.push({
+    path: '',
+    redirectTo: '/accueil',
+    pathMatch: 'full'
+  });
 }
 
 
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes)],
-	exports: [RouterModule],
-	providers: []
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: []
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { AccueilFestyComponent } from './accueil-festy/accueil-festy.component';
+import { AccueilGourmandsComponent } from './accueil-gourmands/accueil-gourmands.component';
+import { AperitifComponent } from './aperitif/aperitif.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AccueilFestyComponent } from './accueil-festy/accueil-festy.component';
-import { ConceptFestyComponent } from './concept-festy/concept-festy.component';
-import { AperitifComponent } from './aperitif/aperitif.component';
-import { MenuFestyComponent } from './menu-festy/menu-festy.component';
-import { MenuLouiseComponent } from './menus/menu-louise/menu-louise.component';
-import { FromagesComponent } from './menus/fromages/fromages.component';
-import { MenuAgatheComponent } from './menus/menu-agathe/menu-agathe.component';
-import { MenuBertilleComponent } from './menus/menu-bertille/menu-bertille.component';
-import { MenuAliceComponent } from './menus/menu-alice/menu-alice.component';
-import { DessertsComponent } from './menus/desserts/desserts.component';
-import { BuffetsComponent } from './buffets/buffets.component';
+import { AutresServicesComponent } from './autres-services/autres-services.component';
+import { BarEpicerieComponent } from './bar-epicerie/bar-epicerie.component';
 import { BuffetJeanneComponent } from './buffets/buffet-jeanne/buffet-jeanne.component';
 import { BuffetJustineComponent } from './buffets/buffet-justine/buffet-justine.component';
 import { BuffetLinaComponent } from './buffets/buffet-lina/buffet-lina.component';
 import { BuffetMarieComponent } from './buffets/buffet-marie/buffet-marie.component';
-import { BuffetAutreComponent } from './buffets/buffet-autre/buffet-autre.component';
-import { SaladesComponent } from './salades/salades.component';
-import { AutresServicesComponent } from './autres-services/autres-services.component';
+import { BuffetsComponent } from './buffets/buffets.component';
+import { ConceptFestyComponent } from './concept-festy/concept-festy.component';
 import { ContactComponent } from './contact/contact.component';
+import { MenuFestyComponent } from './menu-festy/menu-festy.component';
+import { DessertsComponent } from './menus/desserts/desserts.component';
+import { FromagesComponent } from './menus/fromages/fromages.component';
+import { MenuAgatheComponent } from './menus/menu-agathe/menu-agathe.component';
+import { MenuAliceComponent } from './menus/menu-alice/menu-alice.component';
+import { MenuBertilleComponent } from './menus/menu-bertille/menu-bertille.component';
+import { MenuLouiseComponent } from './menus/menu-louise/menu-louise.component';
 import { NavigationFestyComponent } from './navigation-festy/navigation-festy.component';
 import { NavigationGourmandsComponent } from './navigation-gourmands/navigation-gourmands.component';
-import { AccueilGourmandsComponent } from './accueil-gourmands/accueil-gourmands.component';
-import { RepasOuvriersComponent } from './repas-ouvriers/repas-ouvriers.component';
-import { BarEpicerieComponent } from './bar-epicerie/bar-epicerie.component';
 import { RepasGroupesComponent } from './repas-groupes/repas-groupes.component';
+import { RepasOuvriersComponent } from './repas-ouvriers/repas-ouvriers.component';
+import { SaladesComponent } from './salades/salades.component';
+
 
 @NgModule({
   declarations: [
@@ -49,7 +48,6 @@ import { RepasGroupesComponent } from './repas-groupes/repas-groupes.component';
     BuffetJustineComponent,
     BuffetLinaComponent,
     BuffetMarieComponent,
-    BuffetAutreComponent,
     SaladesComponent,
     AutresServicesComponent,
     ContactComponent,
